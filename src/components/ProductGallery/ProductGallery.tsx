24373c2068829d398029fe7830c1a49c402043be
xvandev <xvandev@gmail.com>
2023-11-29 17:23:20 +0100

    first

---
+++ src/components/ProductGallery/ProductGallery.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+export interface ButtonProps {
+    label: string;
+}
+const ProductGallery = (props: ButtonProps) => {
+    return <div>test</div>;
+};
+export default ProductGallery;
